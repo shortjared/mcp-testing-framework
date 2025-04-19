e9a95af36667a5e0c914acaef33c824c90c66f89
qun <778157949@qq.com>
2025-04-19 11:47:05 +0800

    feat: configure lint-and-type workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
-require('@rushstack/eslint-patch/modern-module-resolution');
+require('@rushstack/eslint-patch/modern-module-resolution')
 module.exports = {
   extends: ['@rushstack/eslint-config/profile/node'],
+  ignorePatterns: ['examples'],
   parserOptions: { tsconfigRootDir: __dirname },
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
   },
-};
+}
